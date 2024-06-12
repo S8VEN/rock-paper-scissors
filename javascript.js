9dfb168445fe4bfbf09d198953511ee94a2247e8
Tusn <dedetusnendi@gmail.com>
2024-06-12 16:12:00 +0700

    Add loop playGames 5x and print final score

--- javascript.js
+++ javascript.js
@@ -16,40 +16,45 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-// write variables to keep track of the players score named humanScore and computerScore in the global scope, Initialize those variables with the value of 0.
-let humanScore = 0;
-let computerScore = 0;
-
-// write logic to play single round name playRound
-// define two parameters for playRound; humanChoice and computerChoice as arguments
-function playRound(humanChoice, computerChoice) {
-  // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-  // Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-  // Increment the humanScore or computerScore variable based on the round winner.
-
-  if (humanChoice == "rock" && computerChoice == "scissor") {
-    //rock beat scissor
-    console.log("You win! Rock beats Scissor");
-    humanScore++;
-  } else if (humanChoice == "scissor" && computerChoice == "paper") {
-    //scissor beat paper
-    console.log("You win! Scissor beats Paper");
-    humanScore++;
-  } else if (humanChoice == "paper" && computerChoice == "rock") {
-    //paper beat rock
-    console.log("You win! Paper beats Rock");
-    humanScore++;
-  } else if (humanChoice == computerChoice) {
-    //draw if choice is identical
-    console.log("It is a draw!");
-  } else {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    computerScore++;
+function playGame() {
+  // write variables to keep track of the players score named humanScore and computerScore in the global scope, Initialize those variables with the value of 0.
+  let humanScore = 0;
+  let computerScore = 0;
+  // write logic to play single round name playRound
+  // define two parameters for playRound; humanChoice and computerChoice as arguments
+  function playRound(humanChoice, computerChoice) {
+    // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+    // Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+    // Increment the humanScore or computerScore variable based on the round winner.
+    if (humanChoice == "rock" && computerChoice == "scissor") {
+      //rock beat scissor
+      console.log("You win! Rock beats Scissor");
+      humanScore++;
+    } else if (humanChoice == "scissor" && computerChoice == "paper") {
+      //scissor beat paper
+      console.log("You win! Scissor beats Paper");
+      humanScore++;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+      //paper beat rock
+      console.log("You win! Paper beats Rock");
+      humanScore++;
+    } else if (humanChoice == computerChoice) {
+      //draw if choice is identical
+      console.log("It is a draw!");
+    } else {
+      console.log("You lose! " + computerChoice + " beats " + humanChoice);
+      computerScore++;
+    }
   }
+  //playgame 5 times
+  for (let i=0; i < 5; i++){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  //show final score
+  console.log("Final score:")
+  console.log("You: " + humanScore);
+  console.log("Computer: " + computerScore);
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
+playGame();
